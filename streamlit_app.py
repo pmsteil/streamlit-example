895b42a42ed67f1f6cfbfc78bbceea211c00d91c
Patrick Steil <patrick@infranet.com>
2023-09-09 15:02:02 -0500

    Update to flowchart

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,42 +1,28 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+from graphviz import Digraph
 
+def draw_flowchart():
+    # Create a new Graphviz Digraph
+    dot = Digraph()
 
-# Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+    # Add nodes to the flowchart
+    dot.node('Step1', 'Step 1: Pre-processing')
+    dot.node('Step2', 'Step 2: Feature Engineering')
+    dot.node('Step3', 'Step 3: Model Training')
+    dot.node('Step4', 'Step 4: Model Evaluation')
+    dot.node('Step5', 'Step 5: Predictions')
 
+    # Add edges between the nodes
+    dot.edge('Step1', 'Step2')
+    dot.edge('Step2', 'Step3')
+    dot.edge('Step3', 'Step4')
+    dot.edge('Step4', 'Step5')
 
-"""
-# Welcome to Streamlit!
+    # Render the flowchart
+    st.graphviz_chart(dot.source)
 
+# Set up a title for your app
+st.title("Code Recipe Flowchart")
 
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Call the draw_flowchart function to generate and display the flowchart
+draw_flowchart()
