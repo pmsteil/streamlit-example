2c3bcb1bf4ce68d601262799cf8cbd5e77afe3ab
Patrick Steil <patrick@infranet.com>
2023-09-09 14:47:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,14 @@ import math
 import pandas as pd
 import streamlit as st
 
+
+# Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+
+
 """
 # Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+
 
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
